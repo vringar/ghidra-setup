312230f059ed2f9bae72ff94477d4676ac6adb4a
vringar <git@zabka.it>
2024-07-28 12:51:04 +0200

    go to fixed branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A falke to build Ghidra with all plugins required for reversing and fuzzing the AMD PSP";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:vringar/nixpkgs/fix/ghidra-extensions";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
